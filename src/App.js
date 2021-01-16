bc125d73d906932691bf86e7ac2f340b2a334863
SmilestheSad <vzheng@gmail.com>
2021-01-15 23:47:16 -0500

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import logo from './logo.svg';
-import './App.less';
 
+import './App.less';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+require('dotenv').config();
+
+
+firebase.initializeApp({
+  apiKey: process.env.API_KEY,
+  authDomain: "uomi-6ffa3.firebaseapp.com",
+  projectId: "uomi-6ffa3",
+  storageBucket: "uomi-6ffa3.appspot.com",
+  messagingSenderId: "1008281417322",
+  appId: "1:1008281417322:web:f1a99ea98dc47d0de1fc13",
+  measurementId: "G-9ZSHHNLGQJ"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 function App() {
   return (
     <div className="App">
