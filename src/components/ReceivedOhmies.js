db585a3ea3aea593684f4127485eba79d3f195dd
Raragyay <let987let987@gmail.com>
2021-01-16 17:09:14 -0500

    debug version for getting user from firestore db

--- src/components/ReceivedOhmies.js
+++ src/components/ReceivedOhmies.js
@@ -1,8 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import OhmiCard from './OhmiCard'
 import { data } from '../testing'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import firebase from 'firebase/app'
+import { useCollection } from 'react-firebase-hooks/firestore'
 
 export default function ReceivedOhmies () {
+  const [user] = useAuthState(firebase.auth())
+  const [db] = useCollection(firebase.firestore()
+    .collection('ohmies')
+    .where('receiver', '==',
+      firebase.firestore().collection('users').doc(user.uid)),
+  )
+  useEffect(() => {
+    if (db === null || db === undefined) {
+      return
+    }
+    db.forEach((doc) => {
+      console.log(doc)
+      console.log(doc.data())
+      console.log(doc.data().receiver.id)
+    })
+  }, [db])
   const [ohmiData, setOhmiData] = useState(data)
 
   const handleClick = (id) => () => {
@@ -23,6 +42,9 @@ export default function ReceivedOhmies () {
           />,
         )}
       </div>
+      {db && db.forEach((doc) => {
+        return doc.data().title
+      })}
     </div>
   )
 }
