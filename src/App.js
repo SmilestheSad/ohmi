3165ed884fc188cce365ba39bb80f0bce65b354d
Raragyay <let987let987@gmail.com>
2021-01-16 01:13:12 -0500

    Removed unused import

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ if (!firebase.apps.length) {
   }
 
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content} = Layout;
 
 function App() {
   const [currentTab, setCurrentTab] = useState("1")
