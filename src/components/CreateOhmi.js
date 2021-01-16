8ab58116c50a6c9caa9febe4284f1b995b85e894
Raragyay <let987let987@gmail.com>
2021-01-16 13:29:33 -0500

    Removed unused imports

--- src/components/CreateOhmi.js
+++ src/components/CreateOhmi.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react'
-import { Form, Input, Button, Select } from 'antd';
-import OhmiCard from './OhmiCard'
-const { Option } = Select;
+import React, { useState } from 'react'
+import { Form, Input, Button } from 'antd'
+
 const layout = {
   labelCol: {
     span: 8,
@@ -9,101 +8,101 @@ const layout = {
   wrapperCol: {
     span: 16,
   },
-};
+}
 const tailLayout = {
   wrapperCol: {
     offset: 8,
     span: 16,
   },
-};
+}
 
-export default function CreateOhmi(){
-    const [cardTo, setTo] = useState("");
-    const [cardFrom, setFrom] = useState("");
-    const [cardTitle, setCardTitle] = useState("");
-    const [cardDesc, setCardDesc] = useState("");
-    const [form] = Form.useForm();
+export default function CreateOhmi () {
+  const [cardTo, setTo] = useState('')
+  const [cardFrom, setFrom] = useState('')
+  const [cardTitle, setCardTitle] = useState('')
+  const [cardDesc, setCardDesc] = useState('')
+  const [form] = Form.useForm()
 
   const onFinish = (values) => {
-    setTo(values.cardTo);
-    setFrom(values.cardFrom);
-    setCardTitle(values.cardTitle);
-    setCardDesc(values.cardDesc);
-    console.log(`to: ${cardTo} from: ${cardFrom} title: ${cardTitle} desc: ${cardDesc}`);
-    <OhmiCard to = {cardTo} from = {cardFrom} title = {cardTitle} desc = {cardDesc}/>
-    form.resetFields();
-  };
+    setTo(values.cardTo)
+    setFrom(values.cardFrom)
+    setCardTitle(values.cardTitle)
+    setCardDesc(values.cardDesc)
+    console.log(
+      `to: ${cardTo} from: ${cardFrom} title: ${cardTitle} desc: ${cardDesc}`)
+    form.resetFields()
+  }
 
   const onReset = () => {
-    form.resetFields();
-  };
+    form.resetFields()
+  }
 
   const onFill = () => {
     form.setFieldsValue({
       note: 'Hello world!',
       gender: 'male',
-    });
-  };
+    })
+  }
 
   return (
     <div>
-    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-      <Form.Item
-        name="cardTo"
-        label="To"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="cardFrom"
-        label="From"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="cardTitle"
-        label="Title"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="cardDesc"
-        label="Description"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button htmlType="button" onClick={onReset}>
-          Reset
-        </Button>
-        <Button type="link" htmlType="button" onClick={onFill}>
-          Fill form
-        </Button>
-      </Form.Item>
-    </Form>
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Form.Item
+          name="cardTo"
+          label="To"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input/>
+        </Form.Item>
+        <Form.Item
+          name="cardFrom"
+          label="From"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input/>
+        </Form.Item>
+        <Form.Item
+          name="cardTitle"
+          label="Title"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input/>
+        </Form.Item>
+        <Form.Item
+          name="cardDesc"
+          label="Description"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input/>
+        </Form.Item>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Button htmlType="button" onClick={onReset}>
+            Reset
+          </Button>
+          <Button type="link" htmlType="button" onClick={onFill}>
+            Fill form
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
-  );
+  )
 };
