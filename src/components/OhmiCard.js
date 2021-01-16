bc1e890e5b11c07521b1d2277080fd934ad86d65
Raragyay <let987let987@gmail.com>
2021-01-16 01:44:35 -0500

    removed unused import and fixed indentation

--- src/components/OhmiCard.js
+++ src/components/OhmiCard.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Card} from 'antd';
 
-export default function OhmiCard(props){
-    return(
-        <Card title="Card title" bordered={false} style={{ width: 300 }}>
+export default function OhmiCard() {
+    return (
+        <Card title="Card title" bordered={false} style={{width: 300}}>
             <p>Card content</p>
             <p>Card content</p>
             <p>Card content</p>
