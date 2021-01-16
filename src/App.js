387f5d0e9a512b947ab3c8774863a580649521f8
SmilestheSad <vzheng@gmail.com>
2021-01-16 01:05:49 -0500

    addcard and fix firebaseapp start

--- src/App.js
+++ src/App.js
@@ -10,16 +10,20 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "uomi-6ffa3.firebaseapp.com",
-    projectId: "uomi-6ffa3",
-    storageBucket: "uomi-6ffa3.appspot.com",
-    messagingSenderId: "1008281417322",
-    appId: "1:1008281417322:web:f1a99ea98dc47d0de1fc13",
-    measurementId: "G-9ZSHHNLGQJ"
-})
+if (!firebase.apps.length) {
+    firebase.initializeApp({   
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: "uomi-6ffa3.firebaseapp.com",
+      projectId: "uomi-6ffa3",
+      storageBucket: "uomi-6ffa3.appspot.com",
+      messagingSenderId: "1008281417322",
+      appId: "1:1008281417322:web:f1a99ea98dc47d0de1fc13",
+      measurementId: "G-9ZSHHNLGQJ"});
+  }else {
+    firebase.app(); // if already initialized, use that one
+  }
 
+  
 const { Header, Content, Footer } = Layout;
 
 function App() {
---
+++ src/components/FavourCard.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import {Card} from 'antd';
+export default function FavourCard(){
+    return(
+        <Card title = "hello" style = {{height: "10rem"}}/>
+    )
+}
