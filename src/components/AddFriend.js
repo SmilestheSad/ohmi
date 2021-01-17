39c361a058a8d4b44a018143c7364e83f1e1a7be
Raragyay <let987let987@gmail.com>
2021-01-17 00:39:09 -0500

    Refactored AddFriend.js

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,29 +1,36 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import firebase from 'firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import {Input, Button} from 'antd'
-export default function AddFriend() {
-    const [friendCodes, setFriendCodes] = useState("");
-    const [user] = useAuthState(firebase.auth());
-    const getFriend = () => {
-        const code = friendCodes;
-        console.log(code);
-        if (user) {
-            firebase.firestore()
-                .collection('users')
-                .doc(user.uid)
-                .update({friends: firebase.firestore.FieldValue.arrayUnion(code)})
-        setFriendCodes("");
-        } else {
-            console.log("usernotfound :(")
-        }
+import { Input, Button } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
 
+export default function AddFriend () {
+  const [friendCodes, setFriendCodes] = useState('')
+  const [submittingCode, setSubmittingCode] = useState(false)
+  const [user] = useAuthState(firebase.auth())
+
+  const onFriendCodeAdd = () => {
+    if (!user) {
+      return //todo: add user message
     }
-    return (
-        <>
-            <Input placeholder="Friend Code" value = {friendCodes} onChange={(e) => setFriendCodes(e.target.value)} />
-            <Button onClick={getFriend}> Add Friend </ Button>
+    const code = friendCodes
+    setSubmittingCode(true)
+    firebase.firestore()
+      .collection('users')
+      .doc(user.uid)
+      .update({ friends: firebase.firestore.FieldValue.arrayUnion(code) })
+      .then(() => setFriendCodes(''))
+      .then(() => setSubmittingCode(false))
 
-        </>
-    )
+  }
+  return (
+    <>
+      <Input placeholder="Friend Code" value={friendCodes}
+             onChange={(e) => setFriendCodes(e.target.value)}/>
+      <Button icon={<UserAddOutlined/>} onClick={onFriendCodeAdd}
+              loading={submittingCode}> Add
+        Friend </ Button>
+
+    </>
+  )
 }
