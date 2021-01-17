31cf0e603d7ba9ac2b55f4c31d736ce22825b1f9
Raragyay <let987let987@gmail.com>
2021-01-17 02:42:31 -0500

    Added functionality to remove friend

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,8 +1,9 @@
 import { useAuthState } from 'react-firebase-hooks/auth'
 import firebase from 'firebase/app'
 import { useCollection } from 'react-firebase-hooks/firestore'
-import { Avatar, List } from 'antd'
+import { Avatar, Button, List, Popconfirm } from 'antd'
 import { useState, useEffect } from 'react'
+import { UserDeleteOutlined } from '@ant-design/icons'
 
 export default function FriendList () {
   const [user] = useAuthState(firebase.auth())
@@ -33,6 +34,13 @@ export default function FriendList () {
     setFriends(
       friendsDB.docs.map(doc => ({ id: doc.id, data: doc.data() })))
   }, [friendsDB])
+
+  const removeFriend = (friendCode) => () => {
+    firebase.firestore().collection('users').doc(user.uid).update({
+      friends: firebase.firestore.FieldValue.arrayRemove(friendCode),
+    })
+  }
+
   return (<>
     <List
       dataSource={friends}
@@ -40,7 +48,16 @@ export default function FriendList () {
       header={'Friends'}
       renderItem={(item) => {
         return (<>
-          <List.Item>
+          <List.Item
+            actions={[
+              <Popconfirm
+                onConfirm={removeFriend(item.data.friendCode)}
+                title={'Are you sure you want to remove your friend?'}
+              >
+                <Button
+                  icon={<UserDeleteOutlined/>}/>
+              </Popconfirm>]}
+          >
             <List.Item.Meta
               avatar={<Avatar
                 src={item.data.photoURL}
@@ -50,8 +67,6 @@ export default function FriendList () {
           </List.Item>
         </>)
       }}
-    >
-
-    </List>
+    />
   </>)
 }
