44e28ef8d93f22a9226a330c52b6c246137c905e
Raragyay <let987let987@gmail.com>
2021-01-16 19:25:15 -0500

    Fixed select sorting with help of anson

--- src/components/CreateOhmi.js
+++ src/components/CreateOhmi.js
@@ -3,7 +3,7 @@ import { Form, Input, Button, Select } from 'antd'
 import firebase from 'firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
-const { Option } = Select;
+const { Option } = Select
 
 const layout = {
   labelCol: {
@@ -20,7 +20,7 @@ const tailLayout = {
   },
 }
 
-export default function CreateOhmi() {
+export default function CreateOhmi () {
   const [user] = useAuthState(firebase.auth())
   const [form] = Form.useForm()
 
@@ -30,7 +30,7 @@ export default function CreateOhmi() {
     const fetchData = async () => {
       const db = firebase.firestore()
       const data = await db.collection('users').get()
-      setUsers(data.docs.map(doc => doc.data()))
+      setUsers(data.docs.map(doc => ({ id: doc.id, data: doc.data() })))
     }
     fetchData()
   }, [])
@@ -57,38 +57,41 @@ export default function CreateOhmi() {
   }
 
   return (
-    <div >
+    <div>
       <Form justify="center" {...layout} form={form} name="control-hooks"
-        onFinish={onFinish}>
-      <Form.Item
+            onFinish={onFinish}>
+        <Form.Item
           name="cardReceiver"
           label="To"
           rules={[{ required: true }]}
         >
           <Select
-          showSearch
-          style={{ width: 200 }}
-          placeholder="Select a user to send an Ohmi to"
-          optionFilterProp="children"
-        >
-          {users.map(user => (
-            <Option value={user.uid}>{user.name}</Option>
-          ))}
-        </Select>
+            showSearch
+            style={{ width: 200 }}
+            placeholder="Select a user to send an Ohmi to"
+            filterOption={(input, option) => {
+              return option.children.toLowerCase()
+                .indexOf(input.toLowerCase()) >= 0
+            }}
+          >
+            {users.map(user => (
+              <Option key={user.id} value={user.id}>{user.data.name}</Option>
+            ))}
+          </Select>
         </Form.Item>
         <Form.Item
           name="cardTitle"
           label="Title"
           rules={[{ required: true }]}
         >
-          <Input />
+          <Input/>
         </Form.Item>
         <Form.Item
           name="cardDesc"
           label="Description"
           rules={[{ required: true }]}
         >
-          <Input />
+          <Input/>
         </Form.Item>
         <Form.Item {...tailLayout}>
           <Button type="primary" htmlType="submit">
@@ -99,6 +102,6 @@ export default function CreateOhmi() {
           </Button>
         </Form.Item>
       </Form>
-    </div >
+    </div>
   )
 };
