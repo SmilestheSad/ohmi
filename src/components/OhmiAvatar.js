7e97d225febbdc5192b8368dcb862723d28b2903
Raragyay <let987let987@gmail.com>
2021-01-16 22:57:32 -0500

    Added tooltip to show user's name when logged in

--- src/components/OhmiAvatar.js
+++ src/components/OhmiAvatar.js
@@ -1,5 +1,5 @@
 import { LoadingOutlined, UserOutlined } from '@ant-design/icons'
-import { Avatar } from 'antd'
+import { Avatar, Tooltip } from 'antd'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import firebase from 'firebase'
 import { useState, useEffect } from 'react'
@@ -7,9 +7,11 @@ import { useState, useEffect } from 'react'
 export default function OhmiAvatar () {
   const [user, loading] = useAuthState(firebase.auth())
   const [photoURL, setPhotoURL] = useState(null)
+  const [name, setName] = useState('')
   useEffect(() => {
     if (!user) {
       setPhotoURL(null)
+      setName('')
       return
     }
     firebase.firestore()
@@ -18,13 +20,17 @@ export default function OhmiAvatar () {
       .get()
       .then((snapshot) => {
         setPhotoURL(snapshot.get('photoURL'))
+        setName(snapshot.get('name'))
       })
   }, [user])
   let component
   if (loading) {
     component = <LoadingOutlined spin={true}/>
   } else if (user) {
-    component = <Avatar src={photoURL} style={{ alignSelf: 'center' }}/>
+    component = <Tooltip title={`Logged in as ${name}`}>
+      <Avatar src={photoURL}
+              style={{ alignSelf: 'center' }}/>
+    </Tooltip>
   } else {
     component = <Avatar style={{ alignSelf: 'center' }} icon={<UserOutlined/>}/>
   }
