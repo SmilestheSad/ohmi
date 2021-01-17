ee8d5d2ea2bf6ab0910c2c37703c9b9ce1eea547
Raragyay <let987let987@gmail.com>
2021-01-16 21:00:21 -0500

    Added friend code generation to check if code already exists

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -3,8 +3,9 @@ import { useState, useEffect } from 'react'
 import firebase from 'firebase/app'
 import { Button, Modal, Space } from 'antd'
 import { GoogleOutlined } from '@ant-design/icons'
-import {sample} from 'lodash'
-export default function LoginButton({ style }) {
+import { sample } from 'lodash'
+
+export default function LoginButton ({ style }) {
   const [user, loading] = useAuthState(firebase.auth())
   const [modalVisible, setModalVisible] = useState(false)
   const db = firebase.firestore()
@@ -13,32 +14,35 @@ export default function LoginButton({ style }) {
   const closeModal = () => { setModalVisible(false) }
 
   const createFriendCode = (length, chars) => {
-    var result = '';
-    for (var i = length; i > 0; --i) result += sample(chars);
-    return result;
+    var result = ''
+    for (var i = length; i > 0; --i) result += sample(chars)
+    return result
   }
 
-  const signInWithGoogle = () => {
-    firebase.auth()
+  const signInWithGoogle = async () => {
+    const result = await firebase.auth()
       .signInWithPopup(new firebase.auth.GoogleAuthProvider())
-      .then((result) => {
-        setModalVisible(false)
-        const setFriend = async () => {
-          const user = await (db.collection('users').doc(result.user.uid)).get()
-          if (user.exists) {
-            console.log('exists!')
-          } else {
-            db.collection('users')
-              .doc(result.user.uid)
-              .set({
-                name: result.user.displayName,
-                photoURL: result.user.photoURL,
-                friendcode: createFriendCode(8, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-              })
-          }
-        }
-        setFriend();
-      })
+    setModalVisible(false)
+    const user = await (db.collection('users').doc(result.user.uid)).get()
+    if (user.exists) {
+      console.log('exists!')
+    } else {
+      let friendCode
+      do {
+        friendCode = createFriendCode(8,
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
+      } while (await db.collection('friendCodes').doc(friendCode).get().exists)
+      await db.collection('users')
+        .doc(result.user.uid)
+        .set({
+          name: result.user.displayName,
+          photoURL: result.user.photoURL,
+          friendCode: friendCode,
+        })
+      await db.collection('friendCodes')
+        .doc(friendCode)
+        .set({ userID: result.user.uid })
+    }
   }
 
   const logOut = () => {
@@ -68,7 +72,7 @@ export default function LoginButton({ style }) {
       <Space direction={'vertical'}>
         <Button
           loading={loading}
-          icon={<GoogleOutlined />}
+          icon={<GoogleOutlined/>}
           onClick={signInWithGoogle}
         >
           Sign in with Google
